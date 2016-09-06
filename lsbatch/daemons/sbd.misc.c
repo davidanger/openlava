319ab178d35bcac215f6a318542fe50c9452cc4d
David Bigagli <david@openlava.org>
2016-09-06 15:24:07 -0400

    Add brackets for clarity.

--- lsbatch/daemons/sbd.misc.c
+++ lsbatch/daemons/sbd.misc.c
@@ -505,9 +505,11 @@ find_bound_core(pid_t pid, int *num)
     }
 
     *num = 0;
-    for (i = 0; i < CPU_SETSIZE; i++)
+    for (i = 0; i < CPU_SETSIZE; i++) {
         if (CPU_ISSET(i, &set))
             (*num)++;
+    }
+
     selected_cores = calloc(*num, sizeof(int));
 
     i = 0;
